1b439a4fc5f82da47315629d0d1d9322aae6cc97
Denis Bernard <db047h@gmail.com>
2022-07-26 04:49:06 +0200

    math/Expm1: ironed out implementation based on Hull & Abrham

--- math/exp.go
+++ math/exp.go
@@ -10,12 +10,39 @@ func Exp(z, x *decimal.Decimal) *decimal.Decimal {
 	return z.Add(Expm1(z, x), one)
 }
 
-var maxExpExp = 1 + int(math.Ceil(math.Log10(math.Ln10*decimal.MaxExp)))
+var maxExpExp = int(math.Ceil(math.Log10(math.Ln10 * decimal.MaxExp)))
 
 func Expm1(z, x *decimal.Decimal) *decimal.Decimal {
+	// Implementation based on:
+	//  T. E. Hull and A. Abrham. 1986. Variable precision exponential function.
+	//  ACM Trans. Math. Softw. 12, 2 (June 1986), 79–91.
+	//  https://doi.org/10.1145/6497.6498
+	//
+	// Other than returning e^x - 1 instead of e^x, this implementation differs
+	// from Hull & Abrham's paper on the following points:
+	//  - The upper bound check for x cannot be done as in the paper. Instead,
+	//    we check that x.exp > 10 since the maximum x for which e^x is finite
+	//    is 0.4945×10^10. For values in-between, the last step where we raise z
+	//	  to the power of 10^exp will just overflow to +Inf as expected.
+	//  - For values of x where |x| <= 0.9^-prec, Hull & Abrham return 1 (0 for
+	//    e^x-1). Here we return x as this would be the result returned if this
+	//    short-circuit was disabled.
+	//	- The test |x| <= 0.9e-prec is replaced by the more practical x.exp<=-int(prec)
+	//	  which is equivalent to |x| <= 0.99999...×10^-prec or |x| <= (1-10^-prec)×10^prec.
+	//	  The error E in this case still satisfies |E| < 10^-prec:
+	//	  (1-10^-prec)×10^-prec / (1 - (1-10^-prec)×10^-prec) < 10^-prec
+	//	  simplifies to 1-10^-prec < 1, which is true for prec > 0.
+	//  - x is normalized to the interval (0, .1) for which the series converge faster.
+	//  - For x.exp < 0, the precision needs to be increased by -exp*2+2 in
+	//	  order to preserve the leading fixed digits. eg.:
+	//    e^(1e-4)-1 = 1.000050001...e-4, Rounded to 5 digits: 1.0001e-4.
+	//	  TODO: Need mathematical proof for p += -exp*2+2. Test results are however
+	//	  correct for the extreme case where -exp=prec-1 with p >= z.prec-exp*2+2.
+
 	if z == x {
 		z = new(decimal.Decimal).SetMode(x.Mode()).SetPrec(x.Prec())
 	}
+
 	prec := z.Prec()
 	if prec == 0 {
 		prec = x.Prec()
@@ -26,48 +53,24 @@ func Expm1(z, x *decimal.Decimal) *decimal.Decimal {
 	if x.IsZero() {
 		return z.SetUint64(0).SetPrec(prec)
 	}
-	if x.IsInf() || exp >= maxExpExp {
+	if x.IsInf() || exp > maxExpExp {
 		if x.Signbit() {
 			return z.SetInt64(-1).SetPrec(prec)
 		}
 		return z.SetInf(false).SetPrec(prec)
 	}
-
-	// Exp(x) <= 10^-prec
-	// x^10exp <= -prec(log(10))
-	//
-
-	// TODO: confirm this and find a way to avoid the NewDecimal.
-	if x.CmpAbs(decimal.NewDecimal(9, -int(prec)-1)) <= 0 {
-		if exp < 0 {
-			return z.Set(x).SetPrec(prec)
-		} else {
-			return z.SetInt64(-1).SetPrec(prec)
-		}
+	// |x.mant×10^x.exp| <= 0.999999999999999...×10^-prec => e^x-1 = x
+	if exp <= -int(prec) {
+		return z.Set(x).SetPrec(prec)
 	}
 
 	var (
-		p    uint
+		p    = prec
 		mode = z.Mode()
-		neg  = x.Signbit()
 		t    = new(decimal.Decimal)
 	)
 
-	// Large negative values require a lot of precision if applying the series
-	// expansion or end up with very wrong results. In order to solve this, we
-	// substitute exp(-x) with 1/exp(x):
-	//	z = e^x - 1
-	//	If x < 0: z = (1/e^-x) - 1
-	//	let E(x) = (e^-x) - 1
-	//	=> z = 1/(E(x)+1) - 1
-	// This second step preserves the property (e^x)-1 = x for |x| < 1×10^prec:
-	//	=> z = 1/(E(x)+1) - (E(x)+1)/(E(x)+1) => z = -E(x) / (E(x) + 1).
-	// This will preserve precision for e^x - 1.
-	if neg {
-		x.Neg(x)
-	}
-
-	// 0 < x < +inf
+	// 0.999..×10^-prec < |x| < 1e10
 
 	// scale x down for x >= 0.1: e^(x×10^n) = (e^x)^(10^n)
 	// Scaling x to be < 0.1 instead of < 1 reduces iterations by about a third.
@@ -77,27 +80,15 @@ func Expm1(z, x *decimal.Decimal) *decimal.Decimal {
 		exp++
 	}
 
-	// 0 < x < 1
+	// 0.999..×10^-prec < |x| < 0.1
 
-	// If |x| < 1×10^1, we need to increase the precision more than DigitsPerWord.
-	// i.e. If prec = 34, e^(1×10^-33) = 1.000000000000000000000000000000001e-33
-	// That's 32 0s. In this extreme case, p = (3*prec) - 2 for expm1T
-	// to return the proper result.
-	// TODO: This is an attempt to scale the precision up only when needed.
-	// Testing showed that this works, but we need to prove that this is correct.
-	// TODO: gate x^exp such that too small exponents return 0 and too large return +Inf
-	// largest number is 1×10^MaxExp. So max X is ln(10^MaxExp) = MaxExp×ln(10). Anything with exp >= 11 is +Inf
-
-	if exp < 0 {
-		if -exp < int(prec) {
-			p = 2*uint(-exp) + 2
-		} else {
-			p = 2
-		}
+	if exp >= 0 {
+		p += uint(exp) + 2
 	} else {
-		p = uint(exp) + 2
+		// 0 < -exp < prec
+		// this is specific to the case of e^x-1 and is not covered by Hull & Abrham.
+		p += 2*uint(-exp) + 2
 	}
-	p += prec
 	if p < 4 {
 		p = 4
 	}
@@ -107,75 +98,65 @@ func Expm1(z, x *decimal.Decimal) *decimal.Decimal {
 
 	expm1T(z, x)
 
-	// Scale back up. With x >= 1, we can safely add 1 and subtract again
-	// without loss of precision in the final result.
+	// Scale back up. If exp > 0, |x| >= 1 and we can safely add 1 and subtract
+	// again without loss of precision in the final result.
 	if exp > 0 {
-		// with exp clamped at 11, upow will not overflow. z may overflow,
-		// but this is expected and will produce +Inf.
-		pow(z, t.Add(z, one), upow(10, uint64(exp)))
-		// z.Sub(z×10^LARGE, 1) does not check that z-1 will not change z's value. Subtract only if needed.
-		// TODO: test edge case and proof
-		if z.MantExp(nil) < int(prec) {
-			z.Sub(z, one)
-		}
 		// restore x
 		x.SetMantExp(x, exp)
-	}
-
-	if neg {
-		x.Neg(x)
+		// with exp clamped at 11, upow will not overflow.
+		pow(z, t.Add(z, one), upow(10, uint64(exp)))
+		// If z.exp is large enough, z.Sub(z, 1) will not change z's value
+		// because of the rounding mode ToNearestEven. On the other hand, if z
+		// is close to 0, it will return -1. In both cases, z.Sub will allocate
+		// and shift very large mantissae to perform the operation (this is to
+		// support other rounding modes but could be optimized).
+		// TODO: add this to the TODO list of the decimal package.
 		if z.IsInf() {
-			return z.SetUint64(0).SetPrec(prec)
+			// nothing to do
+		} else if e := z.MantExp(nil); e < -int(p) {
+			// z too small for z.Sub(z, 1) to return a result != -1:
+			// with p=4, -1+0.9999e-5 = -0.999990001, rounds to -1
+			z.SetInt64(-1)
+		} else if e <= int(p) {
+			// If we subtract a 1 to the right of z.mant, skip the subtraction
+			// with p=4, 1234e1-1 = 1233.9e1, rounded to 4 digits again -> 1234e1 = 0.1234e5
+			z.Sub(z, one)
 		}
-		// same as above, add only if z is small enough.
-		// TODO: test edge case and proof
-		if e := z.MantExp(nil); e <= int(prec) {
-			t.Add(z, one)
-		} else {
-			t.Set(z)
-		}
-		z.Quo(z.Neg(z), t)
 	}
 
 	return z.SetMode(mode).SetPrec(prec)
 }
 
 // expm1T sets z to the rounded value of e^x-1, and returns z.
-// x must be in the open interval (0, 1).
+// |x| must be in the open interval (0, 0.1).
 // expm1T uses the Maclaurin series expansion for e^x-1:
 //
 //  x + x^2/2! + x^3/3! + ...
 //
 // The precision of z must be non zero and the caller is responsible for
 // allocating guard digits and rounding down z.
-//
-// For example, get e^x-1 with the same precision as x:
-//  z.SetPrec(x.Prec()+decimal.DigitsPreWord)
-//  expm1T(z, x).SetPrec(x.Prec())
-//
 func expm1T(z, x *decimal.Decimal) *decimal.Decimal {
 
-	// 0 < x < 0.1
+	// 0 < |x| < 0.1
 
 	var (
-		p   = z.Prec()
-		q   = new(decimal.Decimal).SetUint64(2)
-		xn  = dec(p) // x^n / !q
-		sum = dec(p)
-		t   = dec(p)
+		p  = z.Prec()
+		q  = new(decimal.Decimal).SetUint64(2)
+		xn = dec(p) // x^n / !q
+		t  = dec(p)
 	)
 
 	// term 1 for sum and x^n
-	sum.Set(xn.Set(x))
+	z.Set(xn.Set(x))
 
 	// Maclaurin expansion
 	for {
 		xn.Quo(t.Mul(xn, x), q)
-		// TODO: checking xn should be enough to quit (see z.Sub(z, one) gating above when x < 0)
-		sum.Add(z.Set(sum), xn)
-		if z.Cmp(sum) == 0 {
+		if xn.IsZero() || xn.MantExp(nil) < z.MantExp(nil)-int(p) {
+			// xn too small for z.Add(z, xn) to change z.
 			break
 		}
+		z.Add(z, xn)
 		q.Add(q, one)
 	}
 
